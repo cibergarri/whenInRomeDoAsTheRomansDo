d3a46e11371825cdf7f1212a2cb9b2e9013e7c6e
Garri <cibergarri@hotmail.com>
2020-10-07 00:01:37 +0200

    wip(main): add instance functionality

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
+const { getArabicNumber, getRomanNumber } = require("./util");
+
 function RomanNumber(number) {
   if (!new.target) return new RomanNumber(number);
+
+  if (typeof number === "string") {
+    this._roman = number;
+    this._arabic = getArabicNumber(number);
+  } else {
+    this._arabic = number;
+    this._roman = getRomanNumber(number);
+  }
+
+  this.toInt = function () {
+    return this._arabic;
+  };
+  this.toString = function () {
+    return this._roman;
+  };
+
 }
 
 module.exports = RomanNumber;
