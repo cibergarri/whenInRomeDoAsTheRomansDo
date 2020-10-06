046c4df3eea4dd57636dab2fa7c5ed6bdd79039f
Garri <cibergarri@hotmail.com>
2020-10-07 00:24:32 +0200

    tests(regex): add more tests

--- src/regex.unit.test.js
+++ src/regex.unit.test.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const { romanRegex } = require('./regex');
+const { ROMAN_CHARS_VALUES } = require('./constants');
 
 describe('Regex', () => {
   it('should return a valid regex', () => {
@@ -13,5 +14,21 @@ describe('Regex', () => {
   });
   it('should not validate a invalid roman number (XCXXX)', async () => {
     expect(romanRegex.test('XCXXX')).to.be.eq(false);
+  });
+
+  Object.keys(ROMAN_CHARS_VALUES).forEach((romanChar) => {
+    it(`should return ${romanChar} information in its named group`, async () => {
+      const { groups } = romanRegex.exec(romanChar);
+      expect(groups[romanChar]).to.be.eq(romanChar);
+    });
+  })
+  
+  it('should return information for named groups mixed', () => {
+    const { groups } = romanRegex.exec('MMCMLXXXIV');
+    expect(groups.M).to.be.eq('MM');
+    expect(groups.CM).to.be.eq('CM');
+    expect(groups.L).to.be.eq('L');
+    expect(groups.X).to.be.eq('XXX');
+    expect(groups.IV).to.be.eq('IV');
   });
 });
