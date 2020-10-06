8f1ab731e6a4ae3b7bb49fc9f546133ba0e50334
Garri <cibergarri@hotmail.com>
2020-10-07 01:06:37 +0200

    docs(main): add jsDoc

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-underscore-dangle */
 const { getArabicNumber, getRomanNumber } = require('./util');
 
+/**
+ * Converts numbers from arabic to roman and viceversa
+ * @param {Number|String} number number to initialize the instance
+ */
 function RomanNumber(number) {
   if (!new.target) return new RomanNumber(number);
 
@@ -12,9 +16,15 @@ function RomanNumber(number) {
     this._roman = getRomanNumber(number);
   }
 
+  /**
+   * returns number in arabic format
+   */
   this.toInt = function toInt() {
     return this._arabic;
   };
+  /**
+   * returns number in roman format
+   */
   this.toString = function toString() {
     return this._roman;
   };
